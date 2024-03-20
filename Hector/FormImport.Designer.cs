2ddfbb2a99c1e3acb3fd32869bb511919302ee23
Henri <henrisalard@gmail.com>
2024-03-20 10:55:03 +0100

    rebase commit

--- Hector/FormImport.Designer.cs
+++ Hector/FormImport.Designer.cs
@@ -31,6 +31,8 @@ namespace Hector
         {
             this.button1 = new System.Windows.Forms.Button();
             this.LabelFileName = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -52,11 +54,32 @@ namespace Hector
             this.LabelFileName.TabIndex = 1;
             this.LabelFileName.Text = "Nom du fichier csv";
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(51, 128);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(278, 38);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Integrer en mode écrasement";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(51, 183);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(278, 42);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Integrer en mode ajout";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // FormImport
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(382, 353);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.LabelFileName);
             this.Controls.Add(this.button1);
             this.Name = "FormImport";
@@ -72,5 +95,7 @@ namespace Hector
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label LabelFileName;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
--- Hector/FormImport.cs
+++ Hector/FormImport.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -32,8 +33,37 @@ namespace Hector
 
         private void button1_Click(object sender, EventArgs e)
         {
+            var FileContent = string.Empty;
+            var FilePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "csv files (*.csv)|*.csv";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    FilePath = openFileDialog.FileName;
+
+                    //Read the contents of the file into a stream
+                    var fileStream = openFileDialog.OpenFile();
+
+                    LabelFileName.Text = Path.GetFileName(FilePath);
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                    }
+                }
+            }
+            }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
 
         }
+    }
 
     }
-}
